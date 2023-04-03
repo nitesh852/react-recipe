cb26ce3d98827659ebab392c9a3107c7c5b8f419
nitesh852 <niteshbhatti852@gmail.com>
2023-04-03 13:36:20 +0530

    work done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+import { motion } from "framer-motion";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <motion.div>
+        <Veggie />
+        <Popular />
+    </motion.div>
+  )
+}
+
+
+export default Home
